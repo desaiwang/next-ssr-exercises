bfa625feb366efc2bd69ac7bcf1042d3d0c1058d
Desai Wang <dw584@cornell.edu>
2025-02-26 00:08:31 +0800

    my attempt at exercise 3

--- src/app/exercises/03-interview/page.js
+++ src/app/exercises/03-interview/page.js
@@ -1,32 +1,41 @@
-'use client';
-import React from 'react';
-import { useMediaQuery } from 'react-responsive';
+"use client";
+import React from "react";
+import { useMediaQuery } from "react-responsive";
 
-import Interview from './Interview';
-import './styles.css';
+import Interview from "./Interview";
+import "./styles.css";
 
 function InterviewExercise() {
-  const isDesktop = useMediaQuery({
-    query: '(min-width: 500px)',
+  const [isDesktop, setIsDesktop] = React.useState(false);
+  const queryResults = useMediaQuery({
+    query: "(min-width: 500px)",
   });
+  console.log(queryResults);
+
+  React.useEffect(() => {
+    // const queryResults = useMediaQuery({
+    //   query: "(min-width: 500px)",
+    // });
+
+    // console.log(queryResults);
+    setIsDesktop(queryResults);
+  }, []);
+  // const isDesktop = useMediaQuery({
+  //   query: '(min-width: 500px)',
+  // });
 
   return (
     <main>
       <Interview />
       {isDesktop && (
         <aside>
-          <img
-            src="/gwen-artist.png"
-            alt="Portrait of the artist"
-          />
+          <img src="/gwen-artist.png" alt="Portrait of the artist" />
           <h2>About the Artist</h2>
           <p>
-            Gwen Altaria is a contemporary artist known for
-            her unique blend of traditional oil painting
-            techniques and pop culture references. Born and
-            raised in London, England, Vivi had an early
-            love for both the fine arts and the fantastical
-            worlds of video games.
+            Gwen Altaria is a contemporary artist known for her unique blend of
+            traditional oil painting techniques and pop culture references. Born
+            and raised in London, England, Vivi had an early love for both the
+            fine arts and the fantastical worlds of video games.
           </p>
         </aside>
       )}
